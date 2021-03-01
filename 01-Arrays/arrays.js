f207c3ac4aa1fa7b2e53ae0772baddadc213e649
hassanwasswa <assanwasswa@gmail.com>
2021-03-01 11:42:48 +0100

    exe1 arrays solved

--- 01-Arrays/arrays.js
+++ 01-Arrays/arrays.js
@@ -1,6 +1,16 @@
 // use this array
 var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
+var fruits  = array[2];
+array.sort();
+array.push('Kiwi')
+delete array[0]
+array.reverse()
+console.log(array)
+
+
+
+
 // Access and output Oranges.
 
 // Sort the array in alphabetical order.
