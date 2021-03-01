48c40dacdbc43e6747fdb3f8f1bfe30b3a67d7de
hassanwasswa <assanwasswa@gmail.com>
2021-03-01 18:23:26 +0100

    done

--- 04-Array-looping/array-loop.js
+++ 04-Array-looping/array-loop.js
@@ -4,17 +4,21 @@ Sample array : `var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];`
 
 Sample Output : 8 numbers, 5 letters*/
 
-var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
+var array = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3,3,'e',8];
+
+var count_char = 0;
+var count_num = 0;
+console.log(typeof str);
+
+for( var i = 0; i < array.length; i++){
+if(typeof array[i] === "string"){
+    count_char++;
+    
+}else if(typeof array[i] === "number"){
+    count_num++;
+   
+  }
 }
-var numbers = 0;
-var letters = 0;
-arr1.forEach(function(item){
-    if (typeof item === 'number'){
+console.log("The letters are "+count_char);
+console.log("The numbers are "+count_num);
 
-        numbers++;
-    }
-
-    else if (typeof item === 'string'){
-        letters++
-    }
-})
