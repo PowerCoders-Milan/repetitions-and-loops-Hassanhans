99e1d07f38f23852565f52777e15ba21c13378c5
hassanwasswa <assanwasswa@gmail.com>
2021-03-01 16:22:45 +0100

    exe3 multiplication solved

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,18 @@
 // write your code below
+var num2 = 9;
+for (var x = 0; x <= 10; x++) {
+    console.log(x*num2);
+}
+
+
+for (var x = 0; x <= 10; x++) {
+    var result = num2 * x;
+    console.log(num2 + ' * ' + x + ' = ' + result);
+}
+
+
+function num1 (xy){
+    for (var x = 1; x <= 10; x++) {
+        console.log(xy + ' * ' + x + ' = '+ xy*x);
+}}
+num1(2)
